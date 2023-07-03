4e3b88148fc147fc2008e1140dd2a61e1716f27b
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-07-04 04:45:20 +0900

    update

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ app = Flask(__name__)
 shutil.rmtree("temp", ignore_errors=True)
 os.makedirs("temp")
 
+queue = []
 
-@app.route("/")
-def index():
-    now = datetime.now()
-    if os.path.isfile("temp/root-{}-{}.wav".format(now.hour, now.minute)):
+
+def gen(hour, minute):
+    if os.path.isfile("temp/root-{}-{}.wav".format(hour, minute)):
         return send_file(
-            "temp/root-{}-{}.wav".format(now.hour, now.minute), mimetype="audio/wav"
+            "temp/root-{}-{}.wav".format(hour, minute), mimetype="audio/wav"
         )
+    if (hour, minute) in queue:
+        return "이미 생성중입니다."
+    queue.append((hour, minute))
     result = []
-    date1 = number_to_korean1((now.hour - 1) % 12 + 1)
-    date2 = number_to_korean2(now.minute)
+    date1 = number_to_korean1((hour - 1) % 12 + 1)
+    date2 = number_to_korean2(minute)
     texts = ["안녕하세요 지금은 {}시 {}분입니다".format(date1, date2), "오늘의 날씨는 맑음입니다."]
     print(texts)
     for idx, text in enumerate(texts):
@@ -37,16 +40,26 @@ def index():
         # generate alignment for each audio chunk
         plot_alignment(
             np.concatenate(align, axis=0),
-            "temp/root-{}-{}-{}.png".format(now.hour, now.minute, idx),
+            "temp/root-{}-{}-{}.png".format(hour, minute, idx),
             sequence_to_text(text_to_sequence("".join(texts))),
         )
 
+    queue.remove((hour, minute))
     # concatenate all audio chunks
     wav = np.concatenate(result, axis=0)
-    sf.write("temp/root-{}-{}.wav".format(now.hour, now.minute), wav, sample_rate)
-    return send_file(
-        "temp/root-{}-{}.wav".format(now.hour, now.minute), mimetype="audio/wav"
-    )
+    sf.write("temp/root-{}-{}.wav".format(hour, minute), wav, sample_rate)
+    return send_file("temp/root-{}-{}.wav".format(hour, minute), mimetype="audio/wav")
+
+
+@app.route("/")
+def index():
+    now = datetime.now()
+    return gen(now.hour, now.minute)
+
+
+@app.route("/<hour>/<minute>")
+def index2(hour, minute):
+    return gen(int(hour), int(minute))
 
 
 @app.route("/favicon.ico")
@@ -54,28 +67,28 @@ def favicon():
     return ""
 
 
-@app.route("/align/<text>")
-def align(text):
-    if os.path.isfile("temp/{}.png".format(text)):
-        return send_file("temp/{}.png".format(text), mimetype="image/png")
-    wav, align = generate(text)
-    sf.write("temp/{}.wav".format(text), wav, sample_rate)
-    plot_alignment(
-        align, "temp/{}.png".format(text), sequence_to_text(text_to_sequence(text))
-    )
-    return send_file("temp/{}.png".format(text), mimetype="image/png")
+# @app.route("/align/<text>")
+# def align(text):
+#     if os.path.isfile("temp/{}.png".format(text)):
+#         return send_file("temp/{}.png".format(text), mimetype="image/png")
+#     wav, align = generate(text)
+#     sf.write("temp/{}.wav".format(text), wav, sample_rate)
+#     plot_alignment(
+#         align, "temp/{}.png".format(text), sequence_to_text(text_to_sequence(text))
+#     )
+#     return send_file("temp/{}.png".format(text), mimetype="image/png")
 
 
-@app.route("/<text>")
-def hello(text):
-    if os.path.isfile("temp/{}.wav".format(text)):
-        return send_file("temp/{}.wav".format(text), mimetype="audio/wav")
-    wav, align = generate(text)
-    sf.write("temp/{}.wav".format(text), wav, sample_rate)
-    plot_alignment(
-        align, "temp/{}.png".format(text), sequence_to_text(text_to_sequence(text))
-    )
-    return send_file("temp/{}.wav".format(text), mimetype="audio/wav")
+# @app.route("/<text>")
+# def hello(text):
+#     if os.path.isfile("temp/{}.wav".format(text)):
+#         return send_file("temp/{}.wav".format(text), mimetype="audio/wav")
+#     wav, align = generate(text)
+#     sf.write("temp/{}.wav".format(text), wav, sample_rate)
+#     plot_alignment(
+#         align, "temp/{}.png".format(text), sequence_to_text(text_to_sequence(text))
+#     )
+#     return send_file("temp/{}.wav".format(text), mimetype="audio/wav")
 
 
 if __name__ == "__main__":
